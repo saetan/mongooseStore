b74d9f15b4f2b2573e6a61ba4571c0c5301e5cf4
saetan <saetan@live.com>
2021-12-07 13:28:26 +0800

    remove local host setting

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const app = require('./app.js')
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8000;
-const DATABASE = process.env.DATABASE;
-const MONGO_USER = process.env.MONGO_USER;
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_BASE_URL = process.env.MONGO_BASE_URL;
-const MONGO_URL = process.env.MONGO_URI || `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_BASE_URL}/${DATABASE}?retryWrites=true&w=majority`
+const MONGO_URL = process.env.MONGO_URI
 
 mongoose.connect(MONGO_URL).then(() => {
     console.log('database connected')
